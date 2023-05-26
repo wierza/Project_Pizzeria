5d923c8b2f6684073a5baaf02a60154dfb546a61
wierza <129267265+wierza@users.noreply.github.com>
2023-05-26 19:17:56 +0200

    Add server

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -128,7 +128,7 @@ export const select = {
         tableIdAttribute: 'data-table',
     },
       db: {
-        url: '//localhost:3131',
+        url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
         products: 'products',
         orders: 'orders',
         product: 'product',
